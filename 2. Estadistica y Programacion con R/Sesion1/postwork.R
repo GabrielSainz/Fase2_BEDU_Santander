796fa0390f8fffaa29f129e9db5f3ea16e1d0c55
Gabriel Sainz <66332627+GabrielSainz@users.noreply.github.com>
2021-01-09 16:23:24 -0600

    Add files via upload

--- 2. Estadistica y Programacion con R/Sesion1/postwork.R
+++ 2. Estadistica y Programacion con R/Sesion1/postwork.R
@@ -12,13 +12,13 @@
 url <- "https://www.football-data.co.uk/mmz4281/2021/SP1.csv"
 soccer_spain <- read.csv(url)
 head(soccer_spain)
+dim(soccer_spain)
 
 # 2. Del data frame que resulta de importar los datos a R, extrae las columnas que 
 #    contienen los números de goles anotados por los equipos que jugaron en casa 
 #    (FTHG) y los goles anotados por los equipos que jugaron como visitante (FTAG)
 
-home_away_gol <- soccer_spain[, c("Date", "Time", "HomeTeam",
-                 "AwayTeam", "FTHG", "FTAG")]
+home_away_gol <- soccer_spain[, c("HomeTeam","AwayTeam", "FTHG", "FTAG")]
 head(home_away_gol)
 
 # 3.  Consulta cómo funciona la función table en R al ejecutar en la consola ?table
@@ -29,18 +29,18 @@ head(home_away_gol)
       
 # - La probabilidad (marginal) de que el equipo que juega en casa anote x goles 
 #   (x = 0, 1, 2, ...)
-marg_prob_home <- table(home_away_gol$FTHG)
-marg_prob_home/sum(marg_prob_home)
+
+table(home_away_gol$FTHG)/length(home_away_gol$FTHG)*100 # Prob. marginal estimada
 
 # - La probabilidad (marginal) de que el equipo que juega como visitante anote y 
 #   goles (y = 0, 1, 2, ...)
-marg_prob_away <- table(home_away_gol$FTAG)
-marg_prob_away/sum(marg_prob_away)
+
+table(home_away_gol$FTAG)/length(home_away_gol$FTAG)*100 # Prob. marginal estimada
 
 # - La probabilidad (conjunta) de que el equipo que juega en casa anote x goles y 
 #   el equipo que juega como visitante anote y goles (x = 0, 1, 2, ..., y = 0, 1, 2, ...)
 
-table(home_away_gol$FTHG, home_away_gol$FTAG)/sum(table(home_away_gol$FTHG, home_away_gol$FTAG))
+table(home_away_gol$FTHG, home_away_gol$FTAG)/sum(table(home_away_gol$FTHG, home_away_gol$FTAG))*100
 
 
 
